a435afe783659f8a36fd2f36c3930c24c37c7e44
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-03-28 05:08:41 +1300

    Update to latest teapot version.

--- teapot.rb
+++ teapot.rb
@@ -3,15 +3,23 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.5"
+teapot_version "0.7.0"
 
 define_target "dream-examples" do |target|
-	target.install do |environment|
-		Teapot::Build.install_directory(package.path, 'source', environment)
+	target.build do |environment|
+		build_directory(package.path, 'source', environment)
 	end
 	
 	target.depends "Language/C++11"
 	target.depends "Library/Dream"
 	
-	target.provides "Application/dream-examples"
+	target.provides "Application/DreamExamples"
+end
+
+define_configuration "dream-examples" do |configuration|
+	#configuration[:source] = "https://github.com/dream-framework"
+	configuration[:source] = "../"
+	
+	configuration.import! "project"
+	configuration.require "tagged-format"
 end
