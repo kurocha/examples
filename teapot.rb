3935dea30ca09124a3079757187633be8dd302e2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-05-08 01:39:33 +1200

    Bump version.

--- teapot.rb
+++ teapot.rb
@@ -3,9 +3,9 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "0.7.0"
+teapot_version "0.8.0"
 
-define_target "dream-examples" do |target|
+define_target "examples" do |target|
 	target.build do |environment|
 		build_directory(package.path, 'source', environment)
 	end
@@ -13,13 +13,15 @@ define_target "dream-examples" do |target|
 	target.depends "Language/C++11"
 	target.depends "Library/Dream"
 	
-	target.provides "Application/DreamExamples"
+	target.provides "Application/Examples"
 end
 
-define_configuration "dream-examples" do |configuration|
+define_configuration "examples" do |configuration|
 	#configuration[:source] = "https://github.com/dream-framework"
 	configuration[:source] = "../"
 	
 	configuration.import! "project"
 	configuration.require "tagged-format"
+	
+	configuration[:run] = ["Application/Examples"]
 end
