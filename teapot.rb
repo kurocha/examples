23aa0764f9fd5bc8b46492074787659c36a952fc
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-12-11 20:20:58 +1300

    Borked update to `teapot.rb`.

--- teapot.rb
+++ teapot.rb
@@ -3,25 +3,26 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "0.8.0"
+teapot_version "1.0"
 
-define_target "examples" do |target|
+define_target "maze-solver" do |target|
 	target.build do |environment|
-		build_directory(package.path, 'source', environment)
+			source_root = target.package.path + 'source'
+			
+			copy headers: source_root.glob('Maze Solver/**/*.{h,hpp}')
+			
+			build executable: "maze-solver", source_files: source_root.glob('Maze Solver/**/*.cpp')
 	end
 	
 	target.depends "Language/C++11"
 	target.depends "Library/Dream/Client"
 	
-	target.provides "Application/Examples"
+	target.provides "Application/MazeSolver"
 end
 
 define_configuration "examples" do |configuration|
 	configuration[:source] = "https://github.com/dream-framework"
-	#configuration[:source] = "../"
 	
-	configuration.import! "project"
+	configuration.require "project"
 	configuration.require "tagged-format"
-	
-	configuration[:run] = ["Application/Examples"]
 end
